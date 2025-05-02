9f31fe9175c9369a293e2ac9b5e703b9e8e7acb6
Dawit Hailu <Davidhailu0@gmail.com>
2025-05-02 17:49:50 +0300

    add migration file to build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ MIX_ENV=prod mix compile
 MIX_ENV=prod mix assets.deploy
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
+
+_build/prod/rel/my_live_app/bin/my_live_app eval "DeployToRender.Release.migrate"
