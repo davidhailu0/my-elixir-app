903b968331650a9750adec98166c531530772c3d
Dawit Hailu <Davidhailu0@gmail.com>
2025-05-02 16:31:31 +0300

    use a mix build

--- config/runtime.exs
+++ config/runtime.exs
@@ -32,10 +32,6 @@ if config_env() == :prod do
 
   config :my_live_app, MyLiveApp.Repo,
     ssl: [cacertfile: Path.expand("priv/certs/ca.pem")],
-    # ssl_opts: [
-    #   verify: :verify_peer,
-    #   cacertfile: Path.expand("priv/certs/ca.pem")
-    # ],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
